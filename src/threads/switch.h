29ad34acb3ebf7d4c004a347552d92ee82707941
Edison <zx.chen@berkeley.edu>
2022-10-10 05:09:40 +0000

    fix FPU errors

--- src/threads/switch.h
+++ src/threads/switch.h
@@ -8,7 +8,7 @@ struct switch_threads_frame {
   uint32_t esi;                 /*   4: Saved %esi. */
   uint32_t ebp;                 /*   8: Saved %ebp. */
   uint32_t ebx;                 /*  12: Saved %ebx. */
-  // unsigned char fpu_state[108]; /*  16: Saved FPU State (108 bytes). */
+  unsigned char fpu_state[108]; /*  16: Saved FPU State (108 bytes). */
   void (*eip)(void);            /* 124: Return address. */
   struct thread* cur;           /* 128: switch_threads()'s CUR argument. */
   struct thread* next;          /* 132: switch_threads()'s NEXT argument. */
@@ -32,7 +32,7 @@ void switch_thunk(void);
 #endif
 
 /* Offsets used by switch.S. */
-#define SWITCH_CUR 20
-#define SWITCH_NEXT 24
+#define SWITCH_CUR 128
+#define SWITCH_NEXT 132
 
 #endif /* threads/switch.h */
