bbd54026a41f85f1491eec075252feb4a813eb31
Edison <zx.chen@berkeley.edu>
2022-10-10 03:57:15 +0000

    fix bug with startup

--- src/threads/interrupt.h
+++ src/threads/interrupt.h
@@ -31,7 +31,7 @@ struct intr_frame {
   uint16_t fs, : 16;            /* Saved FS segment register. */
   uint16_t es, : 16;            /* Saved ES segment register. */
   uint16_t ds, : 16;            /* Saved DS segment register. */
-  unsigned char fpu_state[108]; /* Saved FPU State (108 bytes). */
+//   unsigned char fpu_state[108]; /* Saved FPU State (108 bytes). */
 
   /* Pushed by intrNN_stub in intr-stubs.S. */
   uint32_t vec_no; /* Interrupt vector number. */
