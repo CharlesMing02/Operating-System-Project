b14505e4f3c7c2f854b5c6f908c55488342bddf9
Andy Chen <andyc789@berkeley.edu>
2022-10-10 21:51:30 +0000

    failing bad sp

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 #include "userprog/process.h"
 #include "threads/synch.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler(struct intr_frame*);
 
@@ -13,6 +14,17 @@ static struct lock global_filesys_lock;
 void syscall_init(void) { 
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
   lock_init(&global_filesys_lock); 
+}
+
+void validate(uint32_t* address) {
+  bool valid = true;
+  for (int i = 0; i < 4; i++) {
+    valid = is_user_vaddr(address+i);
+    if (!valid) {
+      //NOTE: did not set f->eax = -1
+      process_exit(-1);
+    }
+  }
 }
 
 static void syscall_handler(struct intr_frame* f UNUSED) {
@@ -26,24 +38,41 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
 
   /* printf("System call number: %d\n", args[0]); */
 
-  if (args[0] == SYS_EXIT) {
-    f->eax = args[1];
-    //printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
-    process_exit((int) args[1]);
-  } else if (args[0] == SYS_WRITE) {
-    lock_acquire(&global_filesys_lock);
-    if (args[1] == 1) {
-      putbuf(args[2], args[3]);
-      f->eax = args[3];
-    }
-    lock_release(&global_filesys_lock);
-  } else if (args[0] == SYS_PRACTICE) {
-    f->eax = args[1]+1;
-  } else if (args[0] == SYS_HALT) {
-    halt();
-  } else if (args[0] == SYS_EXEC) {
-    f->eax = process_execute((char*) args[1]);
-  } else if (args[0] == SYS_WAIT) {
-    f->eax = process_wait((pid_t) args[1]);   
+  validate((uint32_t*) args);
+  switch (args[0]) {
+    case SYS_EXIT:
+      validate((uint32_t*) &args[1]);
+      f->eax = args[1];
+      //printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
+      process_exit((int) args[1]);
+      break;
+    case SYS_WRITE:
+      validate((uint32_t*) &args[1]);
+      validate((uint32_t*) &args[2]);
+      validate((uint32_t*) args[2]);
+      validate((uint32_t*) &args[3]);
+      lock_acquire(&global_filesys_lock);
+      if (args[1] == 1) {
+        putbuf(args[2], args[3]);
+        f->eax = args[3];
+      }
+      lock_release(&global_filesys_lock);
+      break;
+    case SYS_PRACTICE:
+      validate((uint32_t*) &args[1]);
+      f->eax = args[1]+1;
+      break;
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXEC:
+      validate((uint32_t*) &args[1]);
+      validate((uint32_t*) args[1]);
+      f->eax = process_execute((char*) args[1]);
+      break;
+    case SYS_WAIT:
+      validate((uint32_t*) &args[1]);
+      f->eax = process_wait((pid_t) args[1]);   
+      break;
   }
 }
