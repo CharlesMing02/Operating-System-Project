c491d51f14aa0fc2859a0693712b4a6da1674877
Edison <zx.chen@berkeley.edu>
2022-10-11 05:54:30 +0000

    fix fp-init and fp-kinit

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -180,8 +180,10 @@ tid_t thread_create(const char* name, int priority, thread_func* function, void*
   struct switch_entry_frame* ef;
   struct switch_threads_frame* sf;
   tid_t tid;
+  uint8_t cur_thread_fpu[108];
 
   ASSERT(function != NULL);
+  asm volatile("fsave (%0)" : : "g"(&cur_thread_fpu));
 
   /* Allocate thread. */
   t = palloc_get_page(PAL_ZERO);
@@ -206,6 +208,8 @@ tid_t thread_create(const char* name, int priority, thread_func* function, void*
   sf = alloc_frame(t, sizeof *sf);
   sf->eip = switch_entry;
   sf->ebp = 0;
+  // init and save FPU for sf, restore saved state for cur thread
+  asm volatile("fninit; fsave (%0); frstor (%1)" : : "g"(&sf->fpu_state), "g"(&cur_thread_fpu));
 
   /* Add to run queue. */
   thread_unblock(t);
