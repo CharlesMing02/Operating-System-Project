d355f47c442e0b5732f3f79fdecf1c00d66a5762
Darren Wu <dwu3@berkeley.edu>
2022-10-19 22:34:31 -0700

    staff solution

--- src/tests/userprog/seek-bad-fd.c
+++
@@ -1,9 +0,0 @@
-/* Tries to pass in an invalid file descriptor to the seek syscall. */
-
-#include <syscall.h>
-#include "tests/lib.h"
-#include "tests/main.h"
-
-void test_main(void) {
-    seek(0x20101234, 0);
-}
